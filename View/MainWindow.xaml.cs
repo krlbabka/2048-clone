bbbd2e96fc2ca5744151bb1bee10069d10c7a7ae
Karel Babka <krlbabka@gmail.com>
2023-08-27 21:21:31 +0200

    Created basic layout for MVVM

---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace _2048
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
